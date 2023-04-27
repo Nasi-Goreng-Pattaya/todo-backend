949df09b747c7b7e07909dac0d789f67ca360250
KWT <weitengkoay@gmail.com>
2023-04-27 09:47:59 +0800

    Remove duplicate code

--- src/controls/TaskController.ts
+++ src/controls/TaskController.ts
@@ -6,51 +6,35 @@ import { deleteTask, getTasks, updateTask, addTask } from "../services/TaskServi
 // @route GET /api/task
 // @access Private
 const tryToGetTasks = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const tasks = await getTasks(req.body.user.id)
-        res.json(tasks)
-    } catch (error) {
-        next(error)
-    }   
+    const tasks = await getTasks(req.body.user.id)
+    res.json(tasks)
 })
 
 // @desc Add new task to user's tasks
 // @route POST /api/task
 // @access Private
 const tryToAddTask = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const userId = req.body.user.id
-        delete req.body.user
-        const newTask = await addTask(req.body, userId)
-        res.json(newTask)
-    } catch (error) {
-        next(error)
-    }
+    const userId = req.body.user.id
+    delete req.body.user
+    const newTask = await addTask(req.body, userId)
+    res.json(newTask)
 })
 
 // @desc Update task
 // @route PATCH /api/task/:id
 // @access Private
 const tryToUpdateTask = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        delete req.body.user
-        const updatedTask = await updateTask(req.params.id, req.body)
-        res.json(updatedTask)
-    } catch (error) {
-        next(error)
-    }
+    delete req.body.user
+    const updatedTask = await updateTask(req.params.id, req.body)
+    res.json(updatedTask)
 })
 
 // @desc Delete task
 // @route DELETE /api/task/:id
 // @access Private
 const tryToDeleteTask = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const deletedTask = await deleteTask(req.params.id)
-        res.json(deletedTask)
-    } catch (error) {
-        next(error)
-    }  
+    const deletedTask = await deleteTask(req.params.id)
+    res.json(deletedTask) 
 })
 
 export {
--- src/controls/UserController.ts
+++ src/controls/UserController.ts
@@ -6,11 +6,7 @@ import { deleteUser, loginUser, registerUser, updateUser } from "../services/Use
 // @route GET /api/user/me
 // @access Private
 const getUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        res.json(req.body.user)
-    } catch (error) {
-        next(error)
-    }
+    res.json(req.body.user)
 })
 
 // @desc Authenticate a user
@@ -18,48 +14,32 @@ const getUser = asyncHandler(async (req: Request, res: Response, next: NextFunct
 // @access Public
 const tryToLoginUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body
-    try {
-        const user = await loginUser(email, password)
-        res.json(user)
-    } catch (error) {
-        next(error)
-    }
+    const user = await loginUser(email, password)
+    res.json(user)
 })
 
 // @desc Register user
 // @route POST /api/user/register
 // @access Public
 const tryToRegisterUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const user = await registerUser(req.body)
-        res.json(user)
-    } catch (error) {
-        next(error)
-    }
+    const user = await registerUser(req.body)
+    res.json(user)
 })
 
 // @desc Update user
 // @route PATCH /api/user/:id
 // @access Private
 const tryToUpdateUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const updatedUser = await updateUser(req.params.id, req.body)
-        res.json(updatedUser)
-    } catch (error) {
-        next(error)
-    }
+    const updatedUser = await updateUser(req.params.id, req.body)
+    res.json(updatedUser)
 })
 
 // @desc Delete user
 // @route DELETE /api/user/:id
 // @access Private
 const tryToDeleteUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const deletedUser = await deleteUser(req.params.id)
-        res.json(deletedUser)
-    } catch (error) {
-        next(error)
-    }
+    const deletedUser = await deleteUser(req.params.id)
+    res.json(deletedUser)
 })
 
 export {
