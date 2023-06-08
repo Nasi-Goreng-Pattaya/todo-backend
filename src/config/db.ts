ed3bd376051afcf731f47e5ac31fd1438f7be2cf
KWT <weitengkoay@gmail.com>
2023-06-08 21:48:49 +0800

    Configure cors and Optimize code

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,18 +1,11 @@
 import mongoose from "mongoose";
 import colors from "colors";
-import env from "../utils/validateEnv";
-import app from "../app";
-
-const port = process.env.PORT || 5000;
 
 const connectDB = async (): Promise<void> => {
   try {
     await mongoose.connect(process.env.MONGO_URI as string);
     console.log(
       colors.bgGreen(`MongoDB Connected: ${mongoose.connection.host}`)
-    );
-    app.listen(port, () =>
-      console.log(colors.bgGreen(`Server started on port ${port}`))
     );
   } catch (error) {
     console.error(
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,27 @@
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import express from "express";
+import { userRouter } from "./routes/UserRoutes";
+import { errorHandler } from "./middlewares/ErrorMiddleware";
+import { taskRouter } from "./routes/TaskRoutes";
+import colors from "colors";
+import cors from 'cors';
 
 dotenv.config();
 connectDB();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/user", userRouter);
+app.use("/api/task", taskRouter);
+
+app.use(errorHandler);
+
+app.listen(port, () =>
+    console.log(colors.bgGreen(`Server started on port ${port}`))
+)
